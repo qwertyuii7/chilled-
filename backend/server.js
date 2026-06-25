ca64bddb4ff0af2fa318bd0830b633129730306a
qwertyuii7 <mayankchaudharyetw123@gmail.com>
2026-06-25 18:59:28 +0000

    learned somthing

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,23 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require('cors');
 
-const connectionDB= require("./config/db")
-require( 'dotenv' ).config()
+const connectionDB = require("./config/db")
+require('dotenv').config()
 
 
 const app = express()
+app.use(cors());
 app.use(express.json())
 
 
 connectionDB();
 
-const { shop_router }=require("../backend/routes/shop_routes")
-const { queue_router } = require ("../backend/routes/queue_router")
-const { bookingrouter } = require ("../backend/routes/booking");
-const { joinrouter } = require("../backend/routes/join");
+
+const { queue_router } = require("./routes/queue_router")
+const { bookingrouter } = require("./routes/booking");
+const { joinrouter } = require("./routes/join");
 const { shop_router } = require("./routes/shop_routes")
-
-
-app.use("/join" , joinrouter);
-app.use("/booking",bookingrouter);
-app.use("/queue",queue_router);
-app.use("/shop",shop_router);
-
 
 
 app.get("/", (req, res) => {
@@ -30,7 +25,16 @@ app.get("/", (req, res) => {
 });
 
 
+app.use("/join", joinrouter);
+app.use("/booking", bookingrouter);
+app.use("/queue", queue_router);
+app.use("/shop", shop_router);
 
-app.listen(5000, () => {
-    console.log("server is listening to port 5000")
+
+
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server is listening to port 5000")
 });
