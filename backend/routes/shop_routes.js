3434437c5898565e638cc02d93bec4ff04fb4675
qwertyuii7 <mayankchaudharyetw123@gmail.com>
2026-06-26 10:56:28 +0000

    added route update shop

--- backend/routes/shop_routes.js
+++ backend/routes/shop_routes.js
@@ -8,8 +8,30 @@ shop_router.post("/create", async function (req, res) {
     try {
         const { name, serviceType, shopName, address, phone } = req.body;
 
-        if (!name || !serviceType || !shopName || !address || !phone) {
+        if (
+            !name?.trim() ||
+            !serviceType?.trim() ||
+            !shopName?.trim() ||
+            !address?.trim() ||
+            !phone?.trim()
+        ) {
             return res.status(400).json({ message: "All fields are required" });
+        }
+        if (!/^\d{10}$/.test(phone)) {
+            return res.status(400).json({
+                message: "Invalid phone number"
+            });
+        }
+
+        const existing_shop = await shop_model.findOne({
+            name: name,
+            phone: phone
+        });
+
+        if (existing_shop) {
+            return res.status(400).json({
+                message: "shop already exist"
+            })
         }
 
         const saving_data = await shop_model.create({
@@ -28,13 +50,14 @@ shop_router.post("/create", async function (req, res) {
     } catch (e) {
         return res.status(500).json({
             message: "Error saving data",
-            error: e.message  
+            error: e.message
         });
     }
 });
+
 shop_router.get("/shops", async function (req, res) {
     try {
-        const shops = await shop_model.find().sort({ createdAt: 1 });
+        const shops = await shop_model.find().sort({ createdAt: -1 });
 
         res.status(200).json({
             message: "Registered shops fetched",
@@ -51,17 +74,23 @@ shop_router.get("/shops", async function (req, res) {
 
 shop_router.get("/:shopId", async function (req, res) {
     try {
-        const { shopId } = req.params; 
+        const { shopId } = req.params;
 
-        const findshop = await shop_model.findById(shopId); 
+        if (!mongoose.Types.ObjectId.isValid(shopId)) {
+            return res.status(400).json({
+                message: "Invalid shop id"
+            });
+        }
 
-        if (!findshop) { 
+        const findshop = await shop_model.findById(shopId);
+
+        if (!findshop) {
             return res.status(404).json({
                 message: "Shop doesn't exist"
             });
         }
 
-        res.status(200).json({ 
+        res.status(200).json({
             message: "Shop found!",
             data: findshop
         });
@@ -73,6 +102,79 @@ shop_router.get("/:shopId", async function (req, res) {
         });
     }
 });
+
+
+shop_router.put('/update/:shopId', async function (req, res) {
+    try {
+        const { shopId } = req.params;
+        const { name, serviceType, shopName, address, phone } = req.body;
+
+
+        if (
+            !name?.trim() ||
+            !serviceType?.trim() ||
+            !shopName?.trim() ||
+            !address?.trim() ||
+            !phone?.trim()
+        ) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        if (!/^\d{10}$/.test(phone)) {
+            return res.status(400).json({
+                message: "Invalid phone number"
+            });
+        }
+
+
+        if (!mongoose.Types.ObjectId.isValid(shopId)) {
+            return res.status(400).json({
+                message: "Invalid shop id"
+            });
+        }
+
+        const find_shop = await shop_model.findById(shopId);
+
+        const existingShop = await shop_model.findOne({
+            phone,
+            _id: { $ne: shopId }
+        });
+
+        if (existingShop) {
+            return res.status(400).json({
+                message: "Phone number already in use"
+            });
+        }
+
+        if (!find_shop) {
+            return res.status(404).json({
+                message: "shop doesn't exist"
+            })
+        }
+
+
+        find_shop.name = name;
+        find_shop.serviceType = serviceType;
+        find_shop.shopName = shopName;
+        find_shop.address = address;
+        find_shop.phonne = phone;
+
+
+        await find_shop.save();
+
+        return res.status(200).json({
+            message: "your data is successfully upated",
+            data: find_shop
+        });
+
+    } catch (e) {
+        return res.status(500).json({
+            message: "internal server error",
+            error: e.message
+        });
+    }
+
+});
+
 
 module.exports = {
     shop_router
